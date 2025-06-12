dda116cf5b37d973effaca73ca724f194539990c
Sebastian <59267927+sbublie@users.noreply.github.com>
2025-06-12 19:12:43 +0200

    Update areas

--- types/areas.ts
+++ types/areas.ts
@@ -1,8 +1,15 @@
   // Define area types
   export const AreaTypes = [
-    { value: 'keep_free', label: 'Keep Free', color: '#669999' },
-    { value: 'red', label: 'Zone RED', color: '#ff0000' },
-    { value: 'yellow', label: 'Zone YELLOW', color: '#ffff00' },
-    { value: 'green', label: 'Zone GREEN', color: '#22c55e' },
-    { value: 'heli', label: 'Helicopter Landing Zone', color: '#ff66ff' },
+    { value: 'keep_free', label: 'Freihalten', color: '#669999' },
+    { value: 'red', label: 'Behandlungsplatz Rot', color: '#ff0000' },
+    { value: 'yellow', label: 'Behandlungsplatz Gelb', color: '#ffff00' },
+    { value: 'green', label: 'Behandlungsplatz Grün', color: '#22c55e' },
+    { value: 'green', label: 'Ablageplatz Schwarz/Blau', color: '#22c55e' },
+    { value: 'patient', label: 'Patientenablage', color: '#22c55e' },
+    { value: 'damage', label: 'Schadenstelle', color: '#22c55e' },
+    { value: 'loading', label: 'Ladezone', color: '#22c55e' },
+    { value: 'danger', label: 'Gefahrenstelle', color: '#22c55e' },
+    { value: 'heli', label: 'Landeplatz Helikopter', color: '#ff66ff' },
+    { value: 'rescue', label: 'Rettungsmittelhalteplatz', color: '#ff66ff' },
+
   ];
